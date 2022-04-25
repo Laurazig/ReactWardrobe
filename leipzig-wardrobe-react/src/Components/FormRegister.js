615999421e5dceae4f8cd1f8cdfa6fdd14a29789
Laura <lhzig@protonmail.com>
2022-04-25 17:09:42 +0200

    got name error message working

--- leipzig-wardrobe-react/src/Components/FormRegister.js
+++ leipzig-wardrobe-react/src/Components/FormRegister.js
@@ -1,39 +1,84 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, createRef } from "react";
 
 const FormRegister = () => {
 
-    const [ userName, setUserName ] = useState("")
-    const [ userEmail, setUserEmail ] = useState("")
-    const [ num, setNum ] = useState("")
-    const [ select, setSelect  ] = useState("")
-    const [ comment, setComment ] = useState("")
-    const [ newsLetter, setNewsLetter ] = useState("")
+    const [userName, setUserName] = useState("")
+    const [userEmail, setUserEmail] = useState("")
+    const [num, setNum] = useState("")
+    const [select, setSelect] = useState("")
+    const [comment, setComment] = useState("")
+    const [newsLetter, setNewsLetter] = useState("")
+    const [nameChanged, setNameChanged] = useState(false)
+    const [emailChanged, setEmailChanged] = useState(false)
+    const [numChanged, setNumChanged] = useState(false)
+    const [commentChanged, setCommentChanged] = useState(false)
 
-    const nameErrorDiv = React.createRef();
-    const emailErrorDiv = React.createRef();
 
+    const nameErrorDiv = createRef();
+    const emailErrorDiv = createRef();
+    const numErrorDiv = React.createRef();
+    const commentLengthErrorDiv = React.createRef();
 
-    const updateData = event =>{
-        switch(event.target.userName) {
+    //else does not log
+    useEffect(() => {
+        if (num.length === 0) {
+            console.log("num is empty")
+        } else {
+            console.log("num has some value")
+        }
+    })
+
+    const updateData = event => {
+        switch (event.target.name) {
             case "user_name":
                 setUserName(event.target.value);
+                setNameChanged(true);
+                break;
+            case "user_email":
+                setUserEmail(event.target.value);
+                setEmailChanged(true);
                 break;
             case "num":
                 setNum(event.target.value);
+                setNumChanged(true);
                 break;
             case "select":
                 setSelect(event.target.value);
                 break;
             case "comment":
                 setComment(event.target.value);
+                setCommentChanged(true);
                 break;
             case "newsLetter":
                 setNewsLetter(event.target.value);
                 break;
+            default:
+                break;
         }
     }
 
-    const userData = event =>{
+    const checkEmailFormat = () => {
+        if(/^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/.test(userEmail)) {
+            emailErrorDiv.current.className = "errorNotVisible"
+        } else {
+            emailErrorDiv.current.className = "errorVisible"
+        }
+    }
+
+    const checkCommentLength =()=> {
+        if (comment.length < 20) {
+            commentLengthErrorDiv.current.className = "errorNotVisible"
+        } else {
+            commentLengthErrorDiv.current.className = "errorVisible"
+        }
+    }
+
+    // const submitForm = event => {
+    //     event.preventDefault();
+    //     if ( userName.length)
+    // }
+
+    const userData = event => {
         event.preventDefault();
         console.log("userName", userName);
         console.log("num", num);
@@ -48,15 +93,16 @@ const FormRegister = () => {
     }
 
     const checkNameOnBlur = () => {
-        // console.log("the name input lost focus") 
+        console.log("the name input lost focus")
+        console.log(userName.length)
         if (userName.trim().length === 0) {
             nameErrorDiv.current.style.display = "block";
-        }  else {
+        } else {
             nameErrorDiv.current.style.display = "none";
         }
     }
 
-    return(
+    return (
         <div className="divFormReg">
             <form className="formReg" onSubmit={userData} >
                 <h2>Register for a workshop (formRegister.js)</h2>
@@ -70,19 +116,24 @@ const FormRegister = () => {
                     <label htmlFor="user_email">Email: </label>
                     <input id="user_email" name="user_email" onChange={updateData} />
                     <div className="errorVisible" ref={emailErrorDiv}>Please enter your correct email</div>
+
+                    
                 </div>
-                
+
                 <div>
                     <label htmlFor="num">Number of people:</label>
-                    <input id="age" name="age" type="number" onChange={updateData}/>
+                    <input id="num" name="num" type="number" onChange={updateData} />
+                    <div className={num.length === 0 && numChanged ? "errorVisible" : "errorNotVisible"} >Please state how many people are going</div>
+                    <div className={isNaN(num) && numChanged ? "errorVisible" : "errorNotVisible"} >Please enter a number</div>
+                    <div className={num < 10 && numChanged ? "errorVisible" : "errorNotVisible"} >No more than 10 participants per workshop</div>
                 </div>
 
                 <div>
                     <label htmlFor="select" name="select">Workshop:   </label>
                     <select id="select" name="select" onChange={updateData} value={select}>
                         <option value="vhs">Tailoring basics (VHS)</option>
-                        <option value="witt">Alladin trousers (Daniela)</option>       
-                        <option value="hoodie">Hoodie (Susanne)</option>           
+                        <option value="witt">Alladin trousers (Daniela)</option>
+                        <option value="hoodie">Hoodie (Susanne)</option>
                     </select>
                 </div>
 
@@ -90,19 +141,19 @@ const FormRegister = () => {
                     <label htmlFor="comment">Comment:   </label>
                     <textarea id="comment" name="comment" onChange={updateData} value={comment}></textarea>
                 </div>
-                
+
                 <div>
                     <label>Signup for the newsletter !</label>
                     <input className="radio_input radio_input_right" type="radio" name="news_letter" onChange={updateData} value="yes" />
-                    <label className="radio_label">Yes</label> 
-                    <input className="radio_input radio_input_left" type="radio" name="news_letter" onChange={updateData} value="yes"/>
-{/* why is checked on no?  checked={newsLetter !== "yes"}*/}
-                    <label className="radio_label">No</label> 
+                    <label className="radio_label">Yes</label>
+                    <input className="radio_input radio_input_left" type="radio" name="news_letter" onChange={updateData} value="yes" />
+                    {/* why is checked on no?  checked={newsLetter !== "yes"}*/}
+                    <label className="radio_label">No</label>
                 </div>
                 <div className="form_div">
                     <button id="buttonAdd">Submit</button>
                 </div>
-                
+
             </form>
         </div>
     )
--- structure.md
+++ structure.md
@@ -13,10 +13,17 @@
 
 ##issues
 
-FORMREGISTER.js
- 1. {/* why is checked on no? */}
-     checked={newsLetter !== "yes"}
+FORMREGISTER.js -  workshop registration
+ 1. {/* why is checked on no? */} -      checked={newsLetter !== "yes"}
   2.dropdown workshops cant be changed
+
+  3. page loads with error messages
+      3a. onblur console log worked on name
+
+  4. comment cant type
+  5. fill in but > submit and only get empty string
+  6.    useEffect(()=> { //else does not log
+  
 
 
   
